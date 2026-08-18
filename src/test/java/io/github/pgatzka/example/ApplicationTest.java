0dc99ecc1d4b69568ebe5fd7ffae2a7302d59301
Philipp Gatzka <phil.gatzka@gmail.com>
2026-08-18 15:44:36 +0200

    #7 setup spring boot

---
+++ src/test/java/io/github/pgatzka/example/ApplicationTest.java
@@ -0,0 +1,15 @@
+package io.github.pgatzka.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
