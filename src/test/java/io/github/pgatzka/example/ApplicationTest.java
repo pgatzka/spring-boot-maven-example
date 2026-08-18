a3df91798109e212c2d2c2393e2ef91f29123200
Philipp Gatzka <phil.gatzka@gmail.com>
2026-08-18 21:26:59 +0200

    #13 setup testcontainers

--- src/test/java/io/github/pgatzka/example/ApplicationTest.java
+++ src/test/java/io/github/pgatzka/example/ApplicationTest.java
@@ -2,9 +2,11 @@ package io.github.pgatzka.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 public class ApplicationTest {
 
