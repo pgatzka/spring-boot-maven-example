9fdc16cf304e03175fd9fdbd8242babd8ff9bb74
Philipp Gatzka <70256577+pgatzka@users.noreply.github.com>
2026-08-18 21:41:32 +0200

    13 setup jacoco (#22)

---
+++ src/test/java/io/github/pgatzka/example/TestApplication.java
@@ -0,0 +1,11 @@
+package io.github.pgatzka.example;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+    static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
