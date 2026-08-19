3c0837f7d918453f2c829a009f0ccaecf19fe5b5
Philipp Gatzka <phil.gatzka@gmail.com>
2026-08-19 13:11:06 +0200

    #12 apply spotless

--- src/main/java/io/github/pgatzka/example/rest/exception/NotFoundException.java
+++ src/main/java/io/github/pgatzka/example/rest/exception/NotFoundException.java
@@ -4,7 +4,6 @@ package io.github.pgatzka.example.rest.exception;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponseException;
-import org.springframework.web.server.ResponseStatusException;
 
 public abstract class NotFoundException extends ErrorResponseException {
 
